be636e490f5d895e7b55f8032e3726c32838bb26
Bombo <patryk080998@gmail.com>
2025-11-14 14:53:40 +0900

    Fixed typescript errors

--- components/experience/Experience.tsx
+++ components/experience/Experience.tsx
@@ -2,13 +2,14 @@ import { WindowProps } from "@/types/types";
 import Window from "../ui/windows/Window";
 
 export default function ExperienceWindow(props: WindowProps) {
-    const {isOpen, onClose} = props;
+    const {isOpen, onClose, onMinimize} = props;
 
     return (
         <Window
             title="Experience"
             isOpen={isOpen}
             onClose={onClose}
+            onMinimize={onMinimize}
         >
             <div className="text-white">
                 <p>TODO</p>
--- components/projects/Projects.tsx
+++ components/projects/Projects.tsx
@@ -2,13 +2,14 @@ import { WindowProps } from "@/types/types";
 import Window from "../ui/windows/Window";
 
 export default function ProjectsWindow(props: WindowProps) {
-    const {isOpen, onClose} = props;
+    const {isOpen, onClose, onMinimize} = props;
 
     return (
         <Window
             title="Projects"
             isOpen={isOpen}
             onClose={onClose}
+            onMinimize={onMinimize}
         >
             <div className="text-white">
                 <p>TODO</p>
